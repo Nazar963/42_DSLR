982577d5bb967188d3c10e8087f86dca6e188b95
nazar.aljendli <nazar.aljendli@digitalvirgo.com>
2024-06-16 11:44:46 +0200

    fin + 98%

--- logreg_predict.py
+++ logreg_predict.py
@@ -2,18 +2,26 @@ import numpy as np
 import sys
 import os
 import csv
+import pickle
 from logreg_train import forward_pass
 from describe import get_data
+from logreg_train import SelfMadeStandardScaler
 
-def predict(X, weights, biases):
+def calculate_accuracy(true_labels, predicted_labels):
+	correct_predictions = true_labels == predicted_labels
+	accuracy = accuracy = np.mean(correct_predictions)
+	return accuracy
+
+def predict(X, weights, biases, scaler):
 	y_pred = np.zeros((X.shape[0], len(weights)), dtype=float)
+	X_test_scaled = scaler.transform(X)
 	for i, (weight, bias) in enumerate(zip(weights, biases)):
-		for k in range(X.shape[0]):
-			y_pred[k][i] = forward_pass(X[k], weight, bias)
+		for k in range(X_test_scaled.shape[0]):
+			y_pred[k][i] = forward_pass(X_test_scaled[k], weight, bias)[0]
 	max_index = np.argmax(y_pred, axis=1)
 	houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']
 	predicted_houses = [houses[index] for index in max_index]
-	return predicted_houses
+	return predicted_houses, max_index
 
 def load_models(houses):
 	weights, biases = [], []
@@ -29,27 +37,37 @@ def main(fileName):
 		print("File not found.")
 		return
 
-	data = get_data(fileName)[0][1:, [1, 8, 9]]
+	data = get_data(fileName)[0][:, [1, 8, 9, 17, 18]]
 	data = np.where(data == '', np.nan, data)
 	X = np.nan_to_num(data[:, 1:].astype(float))
 	houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']
 
 	try:
 		weights, biases = load_models(houses)
+		with open('scaler.pkl', 'rb') as f:
+			scaler = pickle.load(f)
 	except FileNotFoundError:
 		print("Models not found. Please train the models first.")
 		return
 
 	print("Predicting...")
-	predicted_houses = predict(X, weights, biases)
+	predicted_houses, p = predict(X, weights, biases, scaler)
 	print("Predictions complete.")
-	return predicted_houses
+	return predicted_houses, p
 
 if __name__ == '__main__':
 	if len(sys.argv) != 2:
 		print("Usage: python3 logreg_predict.py <filename>")
 	else:
-		predictions = main(sys.argv[1])
+		predictions, p = main(sys.argv[1])
+
+		# data_new = get_data('datasets-for-accuracy/true_train.csv')[0]
+		# true_labels = data_new[:,1:2]
+		# pred = np.reshape(predictions, (-1, 1))
+		# accuracy = calculate_accuracy(true_labels, pred)
+		# print(f"Accuracy: {accuracy * 100}%")
+		# print("Predictions complete, writing to CSV file...")
+
 		if predictions:
 			with open('houses.csv', mode='w', newline='') as file:
 				writer = csv.writer(file)
--- logreg_train.py
+++ logreg_train.py
@@ -1,61 +1,23 @@
-###################################################
-#        Versione senza limiti di subject         #
-###################################################
-# import pandas as pd
-# import sys
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.metrics import accuracy_score
-# from sklearn.model_selection import train_test_split
-# from sklearn.feature_selection import SelectKBest, chi2, mutual_info_classif
-# from sklearn.preprocessing import LabelEncoder
-
-# if __name__ == '__main__':
-# 	data = pd.read_csv('datasets/dataset_train.csv')
-
-# 	data_clean = data.dropna()
-# 	C = data_clean.drop(['Hogwarts House', 'First Name', 'Last Name', 'Birthday', 'Best Hand'], axis=1)
-# 	u = data_clean['Hogwarts House']
-# 	encoder = LabelEncoder()
-# 	u_encoded = encoder.fit_transform(u)
-# 	selector = SelectKBest(mutual_info_classif, k=2)
-# 	X_new = selector.fit_transform(C, u_encoded)
-# 	mask = selector.get_support()
-# 	best_features = C.columns[mask]
-# 	print(f"Best features: {best_features}")
-
-# 	data = data[['Defense Against the Dark Arts', 'Herbology', 'Hogwarts House']].dropna()
-# 	X = data[['Defense Against the Dark Arts', 'Herbology']]
-# 	y = data['Hogwarts House']
-# 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-# 	model = LogisticRegression(solver='sag', multi_class='multinomial', max_iter=1000)
-# 	model.fit(X_train, y_train)
-# 	test_data = pd.read_csv('datasets/dataset_test.csv')
-# 	test_data = test_data.dropna(subset=['Defense Against the Dark Arts', 'Herbology'])
-# 	features = test_data[['Defense Against the Dark Arts', 'Herbology']]
-# 	predictions = model.predict(features)
-# 	y_pred = model.predict(X_test)
-# 	accuracy = accuracy_score(y_test, y_pred)
-# 	print(f'Accuracy: {accuracy * 100}%')
-# 	output = pd.DataFrame({
-# 		'Index': test_data.index,
-# 		'Hogwarts House': predictions
-# 	})
-# 	output.to_csv('houses.csv', index=False)
-
-###################################################
-#        Versione con limiti di subject           #
-###################################################
-
 import numpy as np
-import pandas as pd
 import sys
 import os
 import pickle
 from describe import get_data
-from sklearn.preprocessing import StandardScaler
-learning_rate = 0.1
-epochs = 1000
-lambda_=0.01
+learning_rate = 0.01
+epochs = 2000
+lambda_= 0.001
+
+class SelfMadeStandardScaler:
+    def fit(self, X):
+        self.mean_ = np.mean(X, axis=0)
+        self.scale_ = np.std(X, axis=0)
+    
+    def transform(self, X):
+        return (X - self.mean_) / self.scale_
+    
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
 
 def initialize_parameters(n_features):
     weights = np.zeros((n_features, 1))
@@ -68,8 +30,8 @@ def compute_loss(y_pred, y_true, weights):
     reg_cost = (lambda_ / (2 * m)) * np.sum(np.square(weights))
     return cost + reg_cost
 
-def update_parameters(weights, bias, dw, db, y_true):
-    weights -= learning_rate * (dw + (lambda_ * weights) / len(y_true))
+def update_parameters(weights, bias, dw, db, m):
+    weights -= learning_rate * (dw + (lambda_ * weights) / m)
     bias -= learning_rate * db
     return weights, bias
 
@@ -89,24 +51,21 @@ def gradient_descent(X, y_true, y_pred):
 def train_model(X_train, y_train, house):
     n_features = X_train.shape[1]
     weights, bias = initialize_parameters(n_features)
-    scaler = StandardScaler()
+    scaler = SelfMadeStandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     y_train = (y_train == house).astype(int).reshape(-1, 1)
-    
     for i in range(epochs):
         y_pred = forward_pass(X_train_scaled, weights, bias)
         loss = compute_loss(y_pred, y_train, weights)
         dw, db = gradient_descent(X_train_scaled, y_train, y_pred)
-        weights, bias = update_parameters(weights, bias, dw, db, y_train)
-        
-        if i % 100 == 0:
-            print(f"Iteration {i}, Loss: {loss}")
-    
+        weights, bias = update_parameters(weights, bias, dw, db, y_train.shape[0])
+        # if i % 100 == 0:
+        #     print(f"Iteration {i}, Loss: {loss}")
     return weights, bias, scaler
 
 if __name__ == '__main__':
 	if len(sys.argv) != 2:
-		print("EHhH che ne dici di mettere il file train giusto?")
+		print("Please provide the training file.")
 		sys.exit(1)
 	dataset_path = sys.argv[1]
 	if not os.path.isfile(dataset_path):
@@ -114,7 +73,7 @@ if __name__ == '__main__':
 		sys.exit(1)
 
 	data = get_data(dataset_path)[0][1:, :]
-	data = data[:, [1, 8, 9]]
+	data = data[:, [1, 8, 9, 17, 18]]
 	data = np.where(data == '', np.nan, data)
 	X = data[:, 1:]
 	X = X.astype(float)
